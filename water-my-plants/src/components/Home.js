b61f01361cd3b233295b0748b75bed7d0b3114a6
Cory Thomas <cory.a.thomas1992@gmail.com>
2020-09-21 16:12:05 -0400

    added styled-components and CSS Grid to create the layout of the Home page/component. Also created the basis for future media queries

---
+++ water-my-plants/src/components/Home.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const StyledDiv = styled.div`
+    .nav {
+        grid-area: nav;
+    };
+    .plants {
+        grid-area: plants;
+        height: 300px;
+    };
+    .sidebar {
+        grid-area: sidebar
+    }
+    .calendar {
+        grid-area: calendar;
+        height: 300px;
+    };
+    .wrapper {
+        display: grid;
+        grid-gap: 1em;
+        grid-template-areas:
+        "nav"
+        "sidebar"
+        "plants"
+        "calendar"
+    };
+
+    @media only screen and ( min-width: 800px ) {
+        .wrapper {
+            grid-template-columns: 20% auto;
+            grid-template-areas:
+                "nav nav"
+                "sidebar plants"
+                "sidebar calendar"
+        };
+    };
+
+    .addPlant {
+        height: 250px;
+    };
+    .profile {
+        height: 375px;
+    };
+    .box {
+        background-color: #444;
+        color: #fff;
+        border-radius: 5px;
+        padding: 10px;
+        margin: 10px;
+        text-align: center;
+    };
+`;
+
+const Home = () => {
+    return (
+        <StyledDiv>
+            <div className='wrapper'>
+                <div className='nav box'>Nav Bar</div>
+                <section className="sidebar">
+                    <div className='profile box'>Profile</div> 
+                    <div className='addPlant box'>Add Plant</div>
+                </section>
+                <div className='plants box'>Plants</div>
+                <div className='calendar box'>Calendar</div>
+            </div>
+        </StyledDiv>
+    )
+};
+
+export default Home;
