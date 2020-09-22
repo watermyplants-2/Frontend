0c7bb7dbdd34e9f73947dd3049ba66dd6babecc2
Suzanne Cabral <suzannecabral@live.com>
2020-09-22 13:01:41 -0700

    Refactor finished

--- water-my-plants/src/components/Login.js
+++ water-my-plants/src/components/Login.js
@@ -1,20 +1,111 @@
-import React, { useEffect } from 'react';
-import loginSchema from './loginSchema'
-import { Link} from "react-router-dom";
+import React, { useEffect, useState } from 'react';
+import { Link}  from "react-router-dom";
+import * as yup from 'yup'
+import schema from './loginSchema'
+import axios from 'axios'
+
+//----------------------------//
+//   Initial Values
+//----------------------------//
+
+const initialFormValues={
+    id:"",
+    username:"",
+    email:"",
+    password:"",
+  }
+const initialFormErrors={
+    id:"",
+    username:"",
+    email:"",
+    password:"",
+    }
+const initialDisabled=true
 
 
+
+//---------------------------------------------
+//   Login Component
+//---------------------------------------------
 const Login = (props) => {
-    const { change, submit, disabled, errors, setSchema } = props
-
 
 
     //----------------------------//
-    //   Event Handlers
+    //   States
     //----------------------------//
 
-    useEffect(()=>{
-        setSchema(loginSchema)
-    },[])
+    const [formValues,setFormValues]=useState(initialFormValues)
+    const [formErrors, setFormErrors]=useState(initialFormErrors)
+    const [disabled, setDisabled]=useState(initialDisabled)
+    
+
+
+    //----------------------------//
+    //   Helpers
+    //----------------------------//
+    const change = (name, value) => {
+        validate(name, value)
+        setFormValues({...formValues,[name]:value})
+      }
+      
+    const loginSubmit = () => {
+        const newLogin = {
+            username:formValues.username.trim(),
+            password:formValues.password.trim()
+        }
+        postLogin(newLogin)
+    }
+
+    const validate = (name, value) => {
+        yup
+          .reach(schema, name)
+          .validate(value)
+          .then(valid => {
+            setFormErrors({...formErrors, [name]: ""})
+          })
+          .catch(err => {
+            // debugger
+            setFormErrors({...formErrors,[name]: err.errors[0]})
+            // console.log(err)
+          })
+      }
+
+      const postLogin=newLogin=>{
+          console.log("Placeholder - post Login", newLogin)
+      }
+
+    // const postLogin = newLogin => {
+
+    // axios.post('https://reqres.in/api/users', newLogin)
+    // //https://reqres.in/api/users
+    
+    //     .then(res=>{
+    //     console.log(res.data)
+    //     })
+    //     .catch(err=>{
+    //     debugger
+    //     console.log(err)
+    //     })
+    //     .finally(()=>{
+    //     setFormValues(initialFormValues)
+    //     document.getElementById('loginForm').reset()
+    //     })
+    
+    // }
+      
+
+    //----------------------------//
+    //   Event Handlers & Effects
+    //----------------------------//
+
+
+    useEffect(() => {
+        schema.isValid(formValues)
+          .then(valid => {
+            setDisabled(!valid)
+          })
+      }, [formValues])
+      
 
     const onChange = evt => {
         const { name,value } = evt.target
@@ -23,7 +114,7 @@ const Login = (props) => {
 
     const onSubmit = evt => {
         evt.preventDefault()
-        submit()
+        loginSubmit()
     }
 
  
@@ -43,7 +134,7 @@ const Login = (props) => {
             </ul>
             <div>
                 <h1>Login:</h1>
-                <form>
+                <form onSubmit={onSubmit}>
                     <label htmlFor="username">Username:
                     <input 
                         name="username" 
@@ -61,9 +152,9 @@ const Login = (props) => {
                     <button disabled={disabled}>Log In</button>
                 </form>
                 <div id="errorFrame">
-                    {errors.username}
-                    {errors.email}
-                    {errors.password}
+                    {formErrors.username}
+                    {formErrors.email}
+                    {formErrors.password}
                 </div>
             </div>
         </>
